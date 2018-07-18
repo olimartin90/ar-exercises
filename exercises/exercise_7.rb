7184ad3bdcea7990080cb5c5394d7dac6b6ed65b
Olivier Martin <olimartin90@gmail.com>
2018-07-18 03:36:57 +0000

    complete ex 7 and validations for store and employee

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+@store_name = gets.chomp
+
+@store_name = Store.create(name: @store_name)
+
+puts @store_name.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,14 @@
 class Store < ActiveRecord::Base
     has_many :employees
+    validates :name, length: { minimum: 3 }
+    validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validate :must_carry_apparel?
+
+    def must_carry_apparel?
+        unless mens_apparel || womens_apparel
+            errors.add(:mens_apparel, "Must have an apparel")
+            errors.add(:womens_apparel, "Must have an apparel")
+        end
+    end
 end
+
